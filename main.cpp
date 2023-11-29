40d427172bfeb85288e8526a7d3e91cfe92090bf
dvkeia <devikak475@gmail.com>
2023-11-29 16:26:49 -0500

    outline of main

--- main.cpp
+++ main.cpp
@@ -9,32 +9,15 @@ using namespace std;
 
 int main()
 {
+    //In main, read in the dataset, store values in the structures
+
+    //first read into hashmap
     HashMap hm;
-    hm.insert("book", "15", "4.2");
-    hm.insert("shirt", "20", "1.34");
-    hm.insert("tshirt", "25", "2.4");
-    cout << hm.searchName("book").first << " " << hm.searchName("tshirt").second << endl;
-    vector<HashMap::HashNode> v;
-    v = hm.searchKeyword("shirt");
-    for(int i = 0; i < v.size(); i++)
+    while(hm.size() < 100000)
     {
-        HashMap::HashNode h = v[i];
-        cout << "Product: " << h.key << " and " << "Price: " << h.value.first << " Rating: " << h.value.second << endl;
+        //read in name, price, rating from file
+        //insert
     }
 
-    vector<HashMap::HashNode> vp;
-    vp = hm.searchKeywordPriceRating("shirt", 21, "price");
-    for(int i = 0; i < vp.size(); i++)
-    {
-        HashMap::HashNode h = vp[i];
-        cout << "Product: " << h.key << " and " << "Price: " << h.value.first << " Rating: " << h.value.second << endl;
-    }
-
-    vector<HashMap::HashNode> vr;
-    vr = hm.searchKeywordPriceRating("shirt", 2, "rating");
-    for(int i = 0; i < vr.size(); i++)
-    {
-        HashMap::HashNode h = vr[i];
-        cout << "Product: " << h.key << " and " << "Price: " << h.value.first << " Rating: " << h.value.second << endl;
-    }
+    //next read into tree
 }
